2a67e02fcb2cfd848f296cdf9268e166480efaf3
Micael Tchapmi <macbook@DNa1c47b1.SUNet>
2022-05-25 12:34:40 -0700

    fixed variable name

--- models/UNet.py
+++ models/UNet.py
@@ -60,7 +60,7 @@ def UNet_step(args, item):
     sqrt_dist = torch.sum((targets - pred)**2, axis=0)
     avg_keypoint_dist = torch.sqrt(torch.mean(sqrt_dist))
     if args.gpus > 0:
-        avg_keypt_dist = avg_keypt_dist.detach().cpu().numpy()
+        avg_keypoint_dist = avg_keypoint_dist.detach().cpu().numpy()
 
     losses = [loss, reconstruction_loss, pred_loss, avg_keypoint_dist]
     outputs = [pred]

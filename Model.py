dc58cde9f8d2ea046bbd415cf081550839ad2a62
Micael Tchapmi <macbook@DNa1c47b1.SUNet>
2022-05-26 11:57:28 -0700

    reduced size of baseline Model for experiment

--- Model.py
+++ Model.py
@@ -19,8 +19,12 @@ class KeypointModel(nn.Module):
 
         self.pool = nn.MaxPool2d(2, 2)
         """
+        """
         self.conv1 = nn.Conv2d(1, 8, kernel_size=3)
         self.fc1 = nn.Linear(8 * 126 * 126, 512)
+        self.fc2 = nn.Linear(512, 6)
+        """
+        self.fc1 = nn.Linear(128*128, 512)
         self.fc2 = nn.Linear(512, 6)
 
         
@@ -36,8 +40,13 @@ class KeypointModel(nn.Module):
         x = F.relu(self.fc2(x))
         out = self.fc3(x)
         """
+        """
         out = F.relu(self.conv1(x))
         out = torch.flatten(out, 1)
+        out = F.relu(self.fc1(out))
+        out = self.fc2(out)
+        """
+        out = torch.flatten(x, 1)
         out = F.relu(self.fc1(out))
         out = self.fc2(out)
         

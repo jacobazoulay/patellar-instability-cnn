f1f3bbe426dd194f05d0e40b8f5130fbc6c3c843
Daniel Lee <dbdaniellee@gmail.com>
2022-06-01 02:27:09 -0700

    Prep x3

--- ModelPreTrained.py
+++ ModelPreTrained.py
@@ -29,7 +29,7 @@ class KeypointPretrainedModel(nn.Module):
 
         #VGG
         self.model.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        self.model.classifier[6] = nn.Linear(in_features=4096, out_features=1000, bias=True)
+        self.model.classifier[6] = nn.Linear(in_features=4096, out_features=6, bias=True)
         
     def forward(self, x):
         #Resnet
